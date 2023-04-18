0d27ab9a48de1d86ff5ed3ebcdd3af52c1853c1d
Frandy Espinal <frandyespinal@gmail.com>
2023-04-18 17:22:30 -0400

    Eliminando archivos basura, carpetas e imagenes, css, ect

--- tooltip-app/mi-plugin/mi-plugin.php
+++
@@ -1,7 +0,0 @@
-<?php
-function mi_shortcode() {
-    $html = file_get_contents('https://raw.githubusercontent.com/FrandyEM/frandyem.github.io/main/index.html'); // Ruta a tu archivo HTML en GitHub
-    return $html;
-}
-add_shortcode( 'mi_shortcode', 'mi_shortcode' ); // Registra el shortcode
-?>
